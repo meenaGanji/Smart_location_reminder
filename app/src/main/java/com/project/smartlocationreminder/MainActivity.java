9f79054a8b7de2296efd0155e57e89f402226b54
meenaGanji <meenarani.samala@gmail.com>
2022-03-23 00:04:42 -0700

    Home Page with navigation Drawer 1.Setup  GoogleMap 2.Setup Radius with seekbar

--- app/src/main/java/com/project/smartlocationreminder/MainActivity.java
+++ app/src/main/java/com/project/smartlocationreminder/MainActivity.java
@@ -1,36 +1,171 @@
 package com.project.smartlocationreminder;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.internal.service.Common;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+import java.security.Permissions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, SeekBar.OnSeekBarChangeListener, View.OnClickListener {
 
     private GoogleMap mMap;
+    boolean isPermissionGranted;
+    SeekBar seekBar;
+    TextView radiusValue;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    TextView userEmail;
+    ImageView homeMenu;
 
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        InitMemberVaribale();
+        seekBar.setOnSeekBarChangeListener(this);
+        GetSeekBarValue();
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        requestPermission();
+        homeMenu.setOnClickListener(this);
+
+
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void InitMemberVaribale() {
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_View);
+        homeMenu = findViewById(R.id.homeMenu);
+        View view = navigationView.inflateHeaderView(R.layout.header_layout);
+
+        seekBar = view.findViewById(R.id.seekBar);
+        radiusValue = view.findViewById(R.id.radiusValue);
+        userEmail = view.findViewById(R.id.userEmail);
+
+        userEmail.setText(FirebaseAuth.getInstance().getCurrentUser().getEmail());
+
+        seekBar.setMin(1);
+        seekBar.setMax(500);
+    }
+
+    @SuppressLint("MissingPermission")
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions()
-                .position(sydney)
-                .title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        if (isPermissionGranted) {
+            mMap.setMyLocationEnabled(true);
+        } else {
+            requestPermission();
+        }
+    }
+
+    private void requestPermission() {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Permission Required")
+                        .setMessage("Location permission required!")
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(MainActivity.this,
+                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                        111);
+                            }
+                        })
+                        .create()
+                        .show();
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        111);
+            }
+        } else {
+            isPermissionGranted = true;
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        isPermissionGranted = true;
+        mMap.setMyLocationEnabled(true);
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+        SaveSeekBarValue(i);
+        GetSeekBarValue();
+    }
+
+    private void SaveSeekBarValue(int value) {
+        SharedPreferences.Editor editor = getSharedPreferences("MY_PREFS_NAME", MODE_PRIVATE).edit();
+        editor.putInt("seekBar", value);
+        editor.apply();
+
+    }
+
+    private void GetSeekBarValue() {
+        SharedPreferences prefs = getSharedPreferences("MY_PREFS_NAME", MODE_PRIVATE);
+        int seekBarValue = prefs.getInt("seekBar", 0);
+        radiusValue.setText(seekBarValue + "m");
+        seekBar.setProgress(seekBarValue);
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.homeMenu) {
+            drawerLayout.openDrawer(GravityCompat.START);
+        }
+
     }
 }
